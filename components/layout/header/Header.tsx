0aad9a3a257b6b2d21d6f3c4ee4a8a15914461f3
Arsgn <blackbarber1323@gmail.com>
2026-01-13 22:48:06 +0600

    .

---
+++ components/layout/header/Header.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { FC } from "react";
+import scss from "./Header.module.scss";
+
+const Header: FC = () => {
+  return (
+    <header className={scss.Header}>
+      <div className="container">
+        <div className={scss.content}>Header</div>
+      </div>
+    </header>
+  );
+};
+export default Header;
---
+++ components/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import { Welcome } from "./sections/Welcome";
+import { FC } from "react";
+
+const HomePage: FC = () => {
+  return (
+    <>
+      <Welcome />
+    </>
+  );
+};
+
+export default HomePage;
